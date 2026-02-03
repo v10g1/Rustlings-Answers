967324ca88f12c0e4722f00951add5547adf0a78
v10g1 <vrisangaur2006@proton.me>
2026-02-03 12:02:13 +0530

    Initial commit with Rustlings answers

---
+++ exercises/09_strings/strings4.rs
@@ -0,0 +1,36 @@
+// Calls of this function should be replaced with calls of `string_slice` or `string`.
+fn placeholder() {}
+
+fn string_slice(arg: &str) {
+    println!("{arg}");
+}
+
+fn string(arg: String) {
+    println!("{arg}");
+}
+
+// TODO: Here are a bunch of values - some are `String`, some are `&str`.
+// Your task is to replace `placeholder(…)` with either `string_slice(…)`
+// or `string(…)` depending on what you think each value is.
+fn main() {
+    string_slice("blue"); // &str
+
+    string("red".to_string()); // String
+
+    string(String::from("hi")); // String
+
+    string("rust is fun!".to_owned()); // String
+
+    string_slice("nice weather".into()); // &str
+
+    string(format!("Interpolation {}", "Station")); // String
+
+    string_slice(&String::from("abc")[0..1]); // &str
+
+    string_slice("  hello there ".trim()); // &str
+
+    string("Happy Monday!".replace("Mon", "Tues")); // String
+
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase()); // String
+}
+
---
+++ exercises/13_error_handling/errors6.rs
@@ -0,0 +1,90 @@
+use std::num::ParseIntError;
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+// A custom error type that we will be using in `PositiveNonzeroInteger::parse`.
+#[derive(PartialEq, Debug)]
+enum ParsePosNonzeroError {
+    Creation(CreationError),
+    ParseInt(ParseIntError),
+}
+
+impl ParsePosNonzeroError {
+    fn from_creation(err: CreationError) -> Self {
+        Self::Creation(err)
+    }
+
+    // FIX: must accept ParseIntError
+    fn from_parse_int(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<Self, CreationError> {
+        match value {
+            x if x < 0 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            x => Ok(Self(x as u64)),
+        }
+    }
+
+    fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
+        // parse() -> Result<i64, ParseIntError>
+        // convert error -> ParsePosNonzeroError
+        let x: i64 = s
+            .parse()
+            .map_err(ParsePosNonzeroError::from_parse_int)?;
+
+        // new() -> Result<Self, CreationError>
+        // convert error -> ParsePosNonzeroError
+        Self::new(x).map_err(ParsePosNonzeroError::from_creation)
+    }
+}
+
+fn main() {
+    // optional experimentation
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_error() {
+        assert!(matches!(
+            PositiveNonzeroInteger::parse("not a number"),
+            Err(ParsePosNonzeroError::ParseInt(_)),
+        ));
+    }
+
+    #[test]
+    fn test_negative() {
+        assert_eq!(
+            PositiveNonzeroInteger::parse("-555"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Negative)),
+        );
+    }
+
+    #[test]
+    fn test_zero() {
+        assert_eq!(
+            PositiveNonzeroInteger::parse("0"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Zero)),
+        );
+    }
+
+    #[test]
+    fn test_positive() {
+        let x = PositiveNonzeroInteger::new(42).unwrap();
+        assert_eq!(x.0, 42);
+        assert_eq!(PositiveNonzeroInteger::parse("42"), Ok(x));
+    }
+}
